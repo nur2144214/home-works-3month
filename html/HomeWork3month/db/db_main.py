4fd8ea1b6d35f35c7ee959d0bdd4ada3181ef0c1
nurislam <nurnasyrdyn@gmail.com>
2024-11-18 16:45:47 +0600

    hw5.2

--- html/HomeWork3month/db/db_main.py
+++ html/HomeWork3month/db/db_main.py
@@ -1,7 +1,5 @@
-# db_main.py
 import sqlite3
 from db import queries
-from db.queries import INSERT_product_details
 
 db = sqlite3.connect('db/store.sqlite3.db')
 cursor = db.cursor()
@@ -11,21 +9,18 @@ async def sql_create():
         print('База данных подключена!')
     cursor.execute(queries.CREATE_TABLE_collection_products)
     cursor.execute(queries.CREATE_TABLE_product_details)
+    db.commit()
+
 async def sql_insert_store_to_collection_products(productid, collection):
     cursor.execute(
-        INSERT_product_details,
+        queries.INSERT_collection_product,
         (productid, collection)
     )
     db.commit()
 
 async def sql_insert_store_to_product_details(productid, category, info_product):
     cursor.execute(
-        INSERT_product_details,
+        queries.INSERT_product_details,
         (productid, category, info_product)
     )
     db.commit()
-
-
-
-
-
--- html/HomeWork3month/db/queries.py
+++ html/HomeWork3month/db/queries.py
@@ -1,25 +1,26 @@
-# queries.py
+# SQL-запросы для создания таблиц и вставки данных
+
 CREATE_TABLE_product_details = """
 CREATE TABLE IF NOT EXISTS product_details (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     productid TEXT,
     category TEXT,
     info_product TEXT
-
+)
+"""
 
 INSERT_product_details = """
-    INSERT INTO products_details (productid, collection) VALUES (?, ?)
+INSERT INTO product_details (productid, category, info_product) VALUES (?, ?, ?)
 """
 
 CREATE_TABLE_collection_products = """
-CREATE TABLE IF NOT EXISTS product_details (
+CREATE TABLE IF NOT EXISTS collection_products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     productid TEXT,
-    colection TEXT
+    collection TEXT
 )
 """
 
 INSERT_collection_product = """
-    INSERT INTO collection (productid)
-    VALUES (?, ?)
+INSERT INTO collection_products (productid, collection) VALUES (?, ?)
 """
